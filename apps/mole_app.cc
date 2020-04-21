259c2f46a5f8af571535331c712b6f67b86a6710
Ashwini Sarvepalli <ashwini1001@gmail.com>
2020-04-21 13:22:41 -0700

    gflag testing completed

--- apps/mole_app.cc
+++ apps/mole_app.cc
@@ -35,10 +35,29 @@ void MyApp::setup() {
 
 void MyApp::update() { }
 
-
+template <typename C>
+void PrintText(const std::string &text, const C &color, const cinder::ivec2 &size,
+               const cinder::vec2 &loc) {
+  cinder::gl::color(color);
+  auto box = cinder::TextBox()
+      .alignment(cinder::TextBox::CENTER)
+      .font(cinder::Font("Arial", 30))
+      .size(size)
+      .color(color)
+      .backgroundColor(cinder::ColorA(0, 0, 0, 0))
+      .text(text);
+  const auto box_size = box.getSize();
+  const cinder::vec2 locp = {loc.x - box_size.x / 2, loc.y - box_size.y / 2};
+  const auto surface = box.render();
+  const auto texture = cinder::gl::Texture::create(surface);
+  cinder::gl::draw(texture, locp);
+}
 
 void MyApp::draw() {
   DrawBackground();
+  const cinder::ivec2 size = {250, 30};
+  const cinder::vec2 loc = {110, 70};
+  PrintText(player_name_, Color(1,0,0), size, loc);
 }
 
 void MyApp::keyDown(KeyEvent event) { }
